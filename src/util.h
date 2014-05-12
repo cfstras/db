83b2efd638206bc85298e53c343ccbac7b120796
Claus Strasburger <claus@strasburger.de>
2014-05-12 23:22:33 +0200

    DISALLOW_COPY_AND_ASSIGN explicit

--- src/util.h
+++ src/util.h
@@ -52,8 +52,8 @@ inline static uint64_t extractPageIDFromTID(TID tid) {
 // A macro to disallow the copy constructor and operator= functions
 // This should be used in the private: declarations for a class
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&);               \
-  void operator=(const TypeName&)
+  TypeName(const TypeName&) = delete;      \
+  void operator=(const TypeName&) = delete
 
 
 // "System-agnostic"
