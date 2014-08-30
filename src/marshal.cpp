672a61ffe9b0cee9690bd8dcfb6764fe1aeacfd1
Claus Strasburger <claus@strasburger.de>
2014-08-30 12:48:33 +0200

    implemented simple marshal/unmarshal

--- src/marshal.cpp
+++ src/marshal.cpp
@@ -1,5 +1,7 @@
 #include "marshal.h"
 
+#include <cassert>
+
 using namespace std;
 
 Marshal::Marshal(Schema::Relation relation) : relation(relation) {
@@ -8,15 +10,72 @@ Marshal::Marshal(Schema::Relation relation) : relation(relation) {
 
 vector<Register*> Marshal::marshal(const Record& source) {
 	vector<Register*> v(relation.attributes.size());
+	for (size_t i=0; i<v.size(); i++) {
+		v[i] = new Register(0);
+	}
 	marshal(source, v);
 	return v;
 }
 
 void Marshal::marshal(const Record& source, vector<Register*> registers) {
-	//TODO
+	const char* d = source.data();
+	for (size_t i = 0; i<registers.size(); i++) {
+		Register* reg = registers[i];
+		Types::Tag type = relation.attributes[i].type;
+		if (type == Types::Tag::Char) {
+			size_t len = *(size_t*)(d);
+			d += sizeof(size_t);
+			const string s(d, len);
+			d += len;
+			reg->setString(s);
+		} else if (type == Types::Tag::Integer) {
+			reg->setLong(*(int64_t*)d);
+			d += sizeof(int64_t);
+		} else {
+			assert(false); // Not implemented
+		}
+	}
 }
 
 Record Marshal::unmarshal(vector<Register*>& registers) {
-	//TODO
-	return Record(0, "");
+	assert(registers.size() == relation.attributes.size());
+	// calc total length
+	size_t length = 0;
+	for (size_t i = 0; i<registers.size(); i++) {
+		const Register* reg = registers[i];
+		const Schema::Relation::Attribute* attr = &relation.attributes[i];
+		Register::Type type = reg->getType();
+		Register::Type schemaType = attr->type == Types::Tag::Integer ?
+				Register::Type::l : Register::Type::s;
+		assert(type == schemaType);
+
+		if (type == Register::Type::s) {
+			length += sizeof(size_t);
+			length += sizeof(reg->getString().size());
+		} else if (type == Register::Type::l) {
+			length += sizeof(int64_t);
+		} else {
+			assert(false); // Not implemented
+		}
+	}
+
+	char data[length];
+	char* d = data;
+	for (size_t i=0; i<registers.size(); i++) {
+		const Register* reg = registers[i];
+		Register::Type type = reg->getType();
+		if (type == Register::Type::s) {
+			const string& s = reg->getString();
+			*(size_t*)(d) = s.length();
+			d += sizeof(size_t);
+			memcpy(d, s.c_str(), s.length());
+			d += s.length();
+		} else if (type == Register::Type::l) {
+			*(int64_t*)(d) = reg->getLong();
+			d += sizeof(int64_t);
+		} else {
+			assert(false); // Not implemented
+		}
+	}
+	return Record(length, data);
 }
--- test/marshal.cpp
+++ test/marshal.cpp
@@ -24,7 +24,8 @@ protected:
 	void eq(vector<Register*> a, vector<Register*> b) {
 		ASSERT_EQ(a.size(), b.size());
 		for (size_t i = 0; i<a.size(); i++) {
-			EXPECT_EQ(a[i], b[i]);
+			EXPECT_EQ(*a[i], *b[i])
+				<<"Expected "<<a[i]->toString()<<", got "<<b[i]->toString();
 		}
 	}
 };
@@ -36,6 +37,23 @@ TEST_F(MarshalTest, String) {
 	vector<Register*> v {&r};
 
 	Record rec = m.unmarshal(v);
+#ifndef SILENT
+	cerr << "Unmarshaled: " << string(rec.data(), rec.len()) << endl;
+#endif
+	auto v2 = m.marshal(rec);
+	eq(v, v2);
+}
+
+TEST_F(MarshalTest, IntString) {
+	auto s = sch("create table bla (int integer, name char(3));");
+	Marshal m(s);
+	Register r1(123L), r2("abc");
+	vector<Register*> v {&r1, &r2};
+
+	Record rec = m.unmarshal(v);
+#ifndef SILENT
+	cerr << "Unmarshaled: " << string(rec.data(), rec.len()) << endl;
+#endif
 	auto v2 = m.marshal(rec);
 	eq(v, v2);
 }
